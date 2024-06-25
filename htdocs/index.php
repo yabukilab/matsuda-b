7165f836cb5bbbaa0d4e1dd7fcb032b8714829a5
tanihiiragi <s2242071rg@s.chibakoudai.jp>
2024-06-25 15:15:28 +0900

    test

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>aaaaaaaaaaaaaaaa
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ゲーム評価システム</title>
