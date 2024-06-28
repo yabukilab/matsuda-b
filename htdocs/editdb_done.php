4dc3b0a98f6774a3723f8a18a31345d9f5619736
Yukki094 <114076472+Yukki094@users.noreply.github.com>
2024-06-28 14:18:22 +0900

    Add files via upload

--- htdocs/editdb_done.php
+++ htdocs/editdb_done.php
@@ -7,38 +7,38 @@
 	<body>
 		<?php
 
-			require_once '_database_conf.php';
-			require_once '_h.php';
+		require_once '_database_conf.php';
+		require_once '_h.php';
 
-			$pro_Title=$_POST['title'];
-			//$=$_POST[''];
-			//$=$_POST[''];
+		$pro_Title=$_POST['title'];
+		//$=$_POST[''];
+		//$=$_POST[''];
 
-			try
+		try{
+			if($pro_Title=='')
 			{
-				$db = new PDO($dsn, $dbUser, $dbPass);
+				print'タイトルが入力されていません。<br />';	
+		}
+		$db = new PDO($dsn, $dbUser, $dbPass);
 				$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-				$sql = 'UPDATE games SET Title=:Title WHERE Title=:Title';
-                $stmt = $db->prepare($sql);
-                $stmt->bindValue(':Title', $pro_Title, PDO::PARAM_STR);
-				//$stmt->bindValue(':price', $pro_price, PDO::PARAM_INT);
-				//$stmt->bindValue(':code', $pro_code, PDO::PARAM_INT);
+				$sql='UPDATE FROM games VALUES :Title';
+				$stmt=$db->prepare($sql);
+				$stmt->bindValue(':Title', $pro_Title, PDO::PARAM_STR);
+				
 				$stmt->execute();
 
 				$db=null;
 
-				print '修正しました。<br />';
+				print '追加しました。<br />';
 
-			}
-			catch(Exception$e)
-			{
-				echo 'エラーが発生しました。内容: ' . h($e->getMessage());
-	 			exit();
-			}
+		}
+		catch(PDOException$e)
+		{
+			echo 'エラーが発生しました。内容: ' . h($e->getMessage());
+			exit();
+		}
 
 		?>
 		<a href="edit.php">戻る</a>
-	</body>
-</html>
