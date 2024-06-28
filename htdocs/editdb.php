a53c44b4f2374c20f93e59999bd9a982393fb39e
Yukki094 <114076472+Yukki094@users.noreply.github.com>
2024-06-28 17:59:58 +0900

    Update editdb.php

--- htdocs/editdb.php
+++ htdocs/editdb.php
@@ -2,79 +2,43 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>情報修正</title>
+		<title>ゲーム情報修正</title>
 	</head>
 	<body>
-		<?php
+	<?php
 
-			require_once '_database_conf.php';
-			require_once '_h.php';
+require_once '_database_conf.php';
+require_once '_h.php';
 
-			if (isset($_GET['Title'])) {
-				$title = $_GET['Title'];
-			} else {
-				$title = null;
-			}
-			if ($title === null) {
-				// Display the input form
-				echo '<form action="" method="GET">';
-				echo 'ゲームタイトル: <input type="text" name="Title"><br>';
-				echo '<input type="submit" value="変更" >';
-				echo '</form>';
-			} else {
+try
+{
+	$db = new PDO($dsn, $dbUser, $dbPass);
+	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-			try
-			{
-				$db = new PDO($dsn, $dbUser, $dbPass);
-				$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$sql='SELECT * FROM games';
+	$stmt=$db->prepare($sql);
+	$stmt->execute();
 
-				$sql = 'DELETE FROM games WHERE Title = :Title';
-					$stmt = $db->prepare($sql);
-					$stmt->bindValue(':Title', $title, PDO::PARAM_INT);
-					$stmt->execute();
+	$db=null;
 
-				$dbh=null;
+	
+	
 
-				$rec=$stmt->fetch(PDO::FETCH_ASSOC);
-
-				if($rec==false)
-				{
-					print'タイトル名が正しくありません。';
-					print'<a href="edit.php">戻る</a>';
-					print '<br />';
-					exit();
-				}
-
-				$title = $rec['Title'];
-				$pro_price = $rec['price'];
-
-			}
-			catch(Exception $e)
-			{
-				echo 'エラーが発生しました。内容: ' . h($e->getMessage());
-	 			exit();
-			}
-		}
+	print '<br />';
+	print '<form method="get" action="editdb_.php">';
+	print 'ゲーム情報修正：GameID';
+	print '<input type="text" name="proGameID" style="width:20px">';
+	print '<input type="submit" value="決定">';
+	print '</form>';
+}
+catch (PDOException $e)
+{
+	echo 'エラーが発生しました。内容: ' . h($e->getMessage());
+	 exit();
+}
 
 
-		?>
-
-		ゲーム情報修正<br />
-		<br />
-
-		<form method="post" action="editdb_done.php">
-
-
-		ゲーム名<br />
-		<input type="text" name="title" style="width:200px" value="<?php print $title; ?>"><br />
-		//価格<br />
-		//<input type="text" name="price" style="width:50px" value="<?php print $pro_price; ?>">円<br /><br />
-
-		<input type="button" onclick="history.back()" value="戻る">
-		<input type="submit" value="ＯＫ">
-
-		</form>
-
-	</body>
+?>
+</body>
 </html>
